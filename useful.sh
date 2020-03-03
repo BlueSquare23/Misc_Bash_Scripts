dad76d67a4ddcc09fb6104f92910a6e849289c6a
BlueSquare23 <johnlradford23@gmail.com>
2020-03-03 02:12:04 -0500

    Added a very useful script

---
+++ useful.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#This is a very useful program
+
+declare -a cowsayz
+i=0
+
+for x in `cowsay -l|grep -v file`
+do 
+	cowsayz[$i]=$x
+	((i++))
+done
+
+rand=$((1 + $RANDOM % (($i - 1))))
+i=0
+
+for x in "${cowsayz[@]}"
+do 
+	if [ $i -eq $rand ]
+	then
+		fortune -s|cowsay -f "${cowsayz[$i]}"
+	fi
+	((i++))
+done
