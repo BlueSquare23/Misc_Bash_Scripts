688d493947590d2ec3cebe27071cfc04f5879cb3
BlueSquare23 <johnlradford23@gmail.com>
2021-09-28 14:20:14 -0400

    Updated output for pid_footBall.sh to show PID number involved in signal exchange.

--- pid_footBall.sh
+++ pid_footBall.sh
@@ -24,7 +24,7 @@ function catchAndReturn(){
 	t=0
 	while [[ $t -le 5 ]] 
 	do
-		trap "echo 'Team $1 caught footBall' >> file.log && sleep 1 && echo 'Team $1 throwing footBall' >> file.log && ./$script_name throwBall $toTeamNum && exit" SIGUSR1
+		trap "echo 'Team $1 caught signal footBall from PPID $PPID' >> file.log && sleep 1 && ./$script_name throwBall $toTeamNum && exit" SIGUSR1
 		sleep 1
 		((t++))
 	done
@@ -43,10 +43,12 @@ function catchAndReturn(){
 # and exits.
 
 function throwBall(){
+	[[ $1 -eq 1 ]] && thrower=2 || thrower=1
 	./$script_name catchAndReturn $1 &
 	export ListenerPid=$!
 	sleep 1
 	kill -10 $ListenerPid
+	echo "Team $thrower throwing signal footBall to child PID $ListenerPid thrower PID $$" >> file.log
 	unset ListenerPid
 	exit
 }
