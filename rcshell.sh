fb9d95ebdada0aa7cfb17b0385f970f8e03d2021
John R <johnlradford23@gmail.com>
2022-12-31 02:42:36 -0500

    Commit rcshell.sh
    * A simple repl shell for Engineer Man's Piston API.

---
+++ rcshell.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+# A simple repl shell for Engineer Man's Piston API.
+
+cleanup(){
+	[[ -f /tmp/data.json ]] && rm /tmp/data.json
+	printf "\n"
+	exit
+}
+
+trap cleanup SIGINT
+
+piston_api_url="https://emkc.org/api/v1/piston/execute"
+
+# Repl = tight while loop.
+function main(){
+    while true; do
+    	echo -n "piston-shell> "
+    	read -r cmd
+        case $cmd in
+          exit|quit|q)
+            cleanup;;
+        esac
+    
+        # Escape double quotes.
+        cmd=$(sed 's/"/\\"/g' <<< $cmd)
+
+    	echo "{\"language\":\"bash\", \"source\":\"$cmd\"}" > /tmp/data.json
+    
+    	curl --silent \
+            -H "Content-Type: application/json" \
+            -d "@/tmp/data.json" \
+            -X POST $piston_api_url | jq -r '.output'
+    done
+}
+
+[[ $1 == "main" ]] && main
+
+# Run it through rlwrap for gnu readline support.
+rlwrap ./$0 main
