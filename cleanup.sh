78746863fc1e69d711aa073a8eff2190aac0671d
John R <johnlradford23@gmail.com>
2022-12-31 02:44:35 -0500

    Tweak cleanup.sh
    * Implmented changes sugested by shellcheck.
    * Added support for cat vs less vs open.
      - Depends on the file_type, term window height, and number of lines in file.

--- cleanup.sh
+++ cleanup.sh
@@ -8,38 +8,52 @@ trap exit SIGINT
 function getFileType(){
 	[[ -z $1 ]] && exit 2
 	[[ -d $1 ]] && echo directory && return
-	[[ -e $1 ]] && echo file && return
+	if [[ -e $1 ]]; then
+        if [[ "$(file "$1")" =~ 'ASCII text' ]]; then
+            echo text_file
+        else
+            echo other_file
+        fi
+        return
+    fi
 	echo "FileType isn't known!"
 }
 
 function preview(){
 	echo "Preview for $1:"
 	echo
-	[[ $fileType = "file" ]] && head $1
-	[[ $fileType = "directory" ]] && ls $1
+	if [[ $file_type = "text_file" ]]; then 
+        if [[ $(wc -l "$1") -gt $(tput lines) ]]; then
+           cat "$1"
+        else
+           less "$1"
+        fi
+    fi
+	[[ $file_type = "other_file" ]] && open "$1"
+	[[ $file_type = "directory" ]] && ls "$1"
 	echo
-	ask $1
+	ask "$1"
 }
 
 function ask(){
-	fileType=`getFileType $1`
+	file_type=$(getFileType "$1")
 
-	echo "Do you want to delete this $fileType, $1?"
+	echo "Do you want to delete this $file_type, $1?"
 	echo "Yes: y/Y"
 	echo "No: n/N"
 	echo "Preview: p/P"
 	echo -n "> "
 
-	read reply
+	read -r reply
 	echo
 	case $reply in
-		y|Y) deleteItems=(${deleteItems[@]} $1) && echo "$1 Marked for deletion."
+		y|Y) deleteItems=("${deleteItems[@]}" "$1") && echo "$1 Marked for deletion."
 		;;
-		p|P) preview $1
+		p|P) preview "$1"
 		;;
 		n|N) echo "Skipping $1."
 		;;
-		*) echo "!!!Input Error!!! Please try again." && ask $1
+		*) echo "!!!Input Error!!! Please try again." && ask "$1"
 		;;
 	esac
 	echo
@@ -50,10 +64,10 @@ function deleteAsk(){
 	echo
 	echo "The following have been marked for deletion:"
 	echo
-	echo ${deleteItems[@]}
+	echo "${deleteItems[@]}"
 	echo
 	echo -n "Would you like to permanently delete them? (y/n): "
-	read reply
+	read -r reply
 
 	case $reply in
 		y|Y) rm -rf "${deleteItems[@]}" && echo "Items deleted!"
@@ -67,11 +81,9 @@ function deleteAsk(){
 }
 
 declare -a deleteItems=()
-OIFS="$IFS"
-IFS=$'\n'
-for fileObj in `ls -1`
-do
-	ask $fileObj
+
+for fileObj in *; do
+	ask "$fileObj"
 done 
 
 deleteAsk
